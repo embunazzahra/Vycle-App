df58465548e58ce7a6abfd0f6050f5c55ebe9e30
embunazzahra <dhauembunazzahra@gmail.com>
2024-10-08 14:28:15 +0700

    Initial Commit

---
+++ VycleTests/VycleTests.swift
@@ -0,0 +1,17 @@
+//
+//  VycleTests.swift
+//  VycleTests
+//
+//  Created by Dhau Embun Azzahra on 08/10/24.
+//
+
+import Testing
+@testable import Vycle
+
+struct VycleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
