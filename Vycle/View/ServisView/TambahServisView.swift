3024ea7abfd0ecf15de7464149724f246913d433
embunazzahra <dhauembunazzahra@gmail.com>
2024-10-09 13:07:22 +0700

    migrasi ke form dan section

--- Vycle/View/ServisView/TambahServisView.swift
+++ Vycle/View/ServisView/TambahServisView.swift
@@ -15,46 +15,32 @@ struct TambahServisView: View {
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            //tanggal servis view
-            VStack(alignment: .leading) {
-                Text("Tanggal servis")
-                    .headline()
-                
-                Button(action: {}){
-                    Text("23 September 2024")
-                        .padding(.vertical,9)
-                        .padding(.horizontal,12)
-                        .background(
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color.neutral.shade300,lineWidth: 1)
-                        )
-                        .foregroundStyle(.grayShade300)
+            Form {
+                Section{
+                    tanggalServisView()
+                        .listRowInsets(.init(top: 8,
+                                             leading: 0,
+                                             bottom: 16,
+                                             trailing: 0))
                 }
-            }
-            
-            
-            //odometer servis view
-            VStack(alignment: .leading) {
-                Text("Kilometer kendaraan")
-                    .headline()
+                Section{
+                    odometerServisView()
+                        .listRowInsets(.init(top: 0,
+                                             leading: 0,
+                                             bottom: 0,
+                                             trailing: 0))
+                }
                 
-                OdometerServisTextField(text: $odometerValue, placeholder: "78.250")
+                Section{
+                    Text("Nama suku cadang")
+                        .headline()
+                        .listRowInsets(.init(top: 0,
+                                             leading: 0,
+                                             bottom: 0,
+                                             trailing: 0))
+                }
                 
-                Text("Berdasarkan tracking kilometer dari kendaraanmu")
-                    .font(.footnote)
-                    .foregroundStyle(.grayShade100)
-                
-            }
-            
-            //pilih suku cadang view
-            VStack(alignment: .leading) {
-                Text("Nama suku cadang")
-                    .headline()
-
-                            
-                //List
-                List {
+                Section{
                     HStack {
                         Image(systemName: "minus.circle.fill")
                             .resizable()
@@ -64,7 +50,7 @@ struct TambahServisView: View {
                         Text("Pilih suku cadang")
                             .font(.subheadline)
                     }
-                    .listRowBackground(Color.neutral.tint300)
+                    
                     Button(action: {
                     }) {
                         HStack {
@@ -77,24 +63,95 @@ struct TambahServisView: View {
                                 .font(.subheadline)
                                 .foregroundStyle(.blue)
                         }
-                    }.listRowBackground(Color.neutral.tint300)
+                    }
                 }
-//                .frame(height: 160)
-                .scrollContentBackground(.hidden)
-                .padding(.horizontal, -20)
-                .padding(.vertical, -35)
+                .listRowBackground(Color.neutral.tint300)
+                
+                Section{
+                    inputFotoView()
+                }
+         
             }
-            
-            //masukkan foto view
-            Text("bagian foto view")
-            
-            
-        }
-        .padding()
-        .padding(.top,8)
+            .scrollContentBackground(.hidden) //hapus warna background
+            .listSectionSpacing(0) //spacing antar section
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .navigationTitle("Tambahkan servis")
         .navigationBarBackButtonHidden(false)
+    }
+    
+    func tanggalServisView() -> some View {
+        VStack(alignment: .leading) {
+            Text("Tanggal servis")
+                .headline()
+            
+            Button(action: {}){
+                Text("23 September 2024")
+                    .padding(.vertical,9)
+                    .padding(.horizontal,12)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.neutral.shade300,lineWidth: 1)
+                    )
+                    .foregroundStyle(.grayShade300)
+            }
+        }
+    }
+    
+    
+    func odometerServisView() -> some View {
+        VStack(alignment: .leading) {
+            Text("Kilometer kendaraan")
+                .headline()
+            
+            OdometerServisTextField(text: $odometerValue, placeholder: "78.250")
+            
+            Text("Berdasarkan tracking kilometer dari kendaraanmu")
+                .font(.footnote)
+                .foregroundStyle(.grayShade100)
+        }
+    }
+    
+    func pilihSukuCadangView() -> some View {
+        VStack(alignment: .leading) {
+            Text("Nama suku cadang")
+                .headline()
+            
+            
+            //List
+            List {
+                HStack {
+                    Image(systemName: "minus.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 23, height: 23)
+                        .foregroundStyle(.persianRed500)
+                    Text("Pilih suku cadang")
+                        .font(.subheadline)
+                }
+                .listRowBackground(Color.neutral.tint300)
+                Button(action: {
+                }) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 23, height: 23)
+                            .foregroundStyle(.green)
+                        Text("Tambahkan suku cadang lain")
+                            .font(.subheadline)
+                            .foregroundStyle(.blue)
+                    }
+                }.listRowBackground(Color.neutral.tint300)
+            }
+            .frame(height: 160)
+            .scrollContentBackground(.hidden)
+            .padding(.horizontal, -20)
+            .padding(.vertical, -35)
+        }
+    }
+    
+    func inputFotoView() -> some View {
+        Text("bagian foto view")
     }
 }
 
