bb002679178e7b1b365aa05fb41bf1370e23990c
Vincent Senjaya <73159146+vincentsenjaya24@users.noreply.github.com>
2024-11-15 14:03:45 +0700

    first commit

---
+++ Vycle/View/AddVehicleView.swift
@@ -0,0 +1,18 @@
+//
+//  AddVehicleView.swift
+//  Vycle
+//
+//  Created by Vincent Senjaya on 14/11/24.
+//
+
+import SwiftUI
+
+struct AddVehicleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddVehicleView()
+}
